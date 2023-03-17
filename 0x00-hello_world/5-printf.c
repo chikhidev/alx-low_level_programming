c26c55817fe9e9fac10c8412eddcf599265a96e9
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-16 17:25:58 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints out a string via printf
+* Description: Program that prints out the string s followed by a new line
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+}
