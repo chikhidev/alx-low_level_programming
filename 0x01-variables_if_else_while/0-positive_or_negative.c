ea34d55c6ecd2889684d736bf1eaefb7ea3657a3
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-17 10:18:01 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+* returns: success
+*/
 
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
