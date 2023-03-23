299d32fcc1dd570d92493bc3db7c5bd569936e2c
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-22 18:13:25 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -18,7 +18,7 @@ int main(void)
         *        putchar(i);
         *}
 	*/
-        printf("Infinite loop avoided! \\o/\n");
+  printf("Infinite loop avoided! \\o/\n");
 
         return (0);
 }
