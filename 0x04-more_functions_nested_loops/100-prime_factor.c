6497dd86f1ad7052a0c7dc1338149273f05f0bdb
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 18:45:17 -0700

    100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,20 +8,22 @@
 
 int main(void)
 {
-	long number = 612852475143;
-	int inc;
+long number = 612852475143;
+int inc;
 
-	while (inc++ < number /2)
-	{
-			if (number % inc == 0)
-				number /= 2;
-				continue;
-			for (inc = 3; inc < number / 2; inc += 2)
-			{
-				if (number % inc == 0)
-					number /= inc;
-			}
-	}
-	printf("%ld\n", number);
-	return (0);
+while (inc++ < number /2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
+}
+}
+printf("%ld\n", number);
+return (0);
 }
