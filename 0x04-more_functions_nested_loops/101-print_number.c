6037fb8a653b014257887b2f139d21d9327b09ac
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 19:15:18 -0700

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,70 +1,22 @@
-#include "main.h"
-#include <limits.h>
+ #include "main.h"
 
 /**
-* print_number - outputs a number
-* @n: an int given from main
+* print_number - Prints a number
+* @n: The number to print
 *
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: nothing void 
 */
-
 void print_number(int n)
 {
-	int x = n;
-	int digit;
-	int places = 1000000000;
-
-	if (n < 0)
-	{
-		x = -n;
-		_putchar('-');
-	}
-	if (n == INT_MAX || n == INT_MIN)
-	{
-		while (1)
-		{
-			if (n == INT_MAX)
-			{
-				_putchar('2');
-				_putchar('1');
-				_putchar('4');
-				_putchar('7');
-				_putchar('4');
-				_putchar('8');
-				_putchar('3');
-				_putchar('6');
-				_putchar('4');
-				_putchar('7');
-				break;
-			}
-			else if (n == INT_MIN)
-			{
-				_putchar('2');
-				_putchar('1');
-				_putchar('4');
-				_putchar('7');
-				_putchar('4');
-				_putchar('8');
-				_putchar('3');
-				_putchar('6');
-				_putchar('4');
-				_putchar('8');
-				break;
-			}
-		}
-	}
-	else if (n == 0)
-		_putchar('0');
-	else
-	{
-		while (places > x)
-			places /= 10;
-		while (places > 0)
-		{
-			digit = x / places;
-			_putchar((digit % 10) + '0');
-			places /= 10;
-		}
-	}
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
 }
+if (num > 9)
+{
+print_number(num / 10);
+}
+_putchar(num % 10 + '0');
+}
+
