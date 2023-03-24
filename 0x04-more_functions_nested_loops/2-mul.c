66d760cf2df3efa20abe0fc9e9710adfe2b37b98
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 17:32:24 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,6 +4,7 @@
 * mul - returns the product of 2 ints 
 * @a: an int given by the main function
 * @b: an int given by the main function
+*
 * Description: Uses headers to link and a nested loops to achieve goal
 * Return: see first line 
 */
