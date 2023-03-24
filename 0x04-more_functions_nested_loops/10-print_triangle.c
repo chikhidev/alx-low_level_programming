6edd73ebd4001831426529032138e21875a1ee3e
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 18:24:32 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,9 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 	for (i = 0; i < size; i++)
-	{	
+	{
 		for (j = size - i - 1; j > 0; j--)
-			_putchar(' ');	
+			_putchar(' ');
 		for (j = i + 1; j > 0; j--)
 			_putchar('#');
 		_putchar('\n');
