48e00f8777285c4197410cbde55c30a9b4b0ece7
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 18:15:22 -0700

    0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - fizz bizz holterton style
+* Return: 0 if no errors
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf(" ");
+	return (0);
+}
