dee6091550b37182669767373806288f86cb4294
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 18:28:50 -0700

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - finds the largest prime
+* Return: 0 if no errors
+*/
+
+int main(void)
+{
+	unsigned long i = 2;
+	unsigned long biggest = 0;
+	unsigned long number = 612852475143;
+
+	while (number > i)
+	{
+		while (number % i == 0)
+		{
+			if (i > biggest)
+				biggest = i;
+			number = number / i;
+		}
+		i++;
+	}
+	printf("%lu\n", biggest);
+	return (0);
+}
