8c738356bce281bc29da9b62352cf298dbaa0b30
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 17:26:22 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,14 +5,11 @@
 * _isdigit - checkes if c is digit
 * @c: variable param
 * Return: 1 if true 0 if false
-*
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: see first line
 */
 
 int _isdigit(int c)
 {
-	if(c >= 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
