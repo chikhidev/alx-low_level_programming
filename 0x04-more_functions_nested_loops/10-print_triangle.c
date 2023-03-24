edc99f517f8ed88e4024eb9f23f63214f7c82a72
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-23 18:22:40 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_triangle - void function
+* @size: int param
+* Return: void
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{	
+		for (j = size - i - 1; j > 0; j--)
+			_putchar(' ');	
+		for (j = i + 1; j > 0; j--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
