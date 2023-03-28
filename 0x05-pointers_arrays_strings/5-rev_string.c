96a80ed108bc0b3cbe94a314bc74aed0886a45f5
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-27 17:51:24 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * rev_string - reverses a string
+ * _putchar - print each character
  * @s: char to check
  *
+ * Description: This will reverse a string
  * Return: 0 is success
  */
 void rev_string(char *s)
