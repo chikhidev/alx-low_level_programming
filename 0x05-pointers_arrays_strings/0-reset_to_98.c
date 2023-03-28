efd0a1b52c750c26a895902a033df315b53f2ec7
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-27 17:12:16 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
-* reset_to_98 - returns void
-* @*n: pointer of int as param
-*/
-
+ * reset_to_98 - takes the pointer and updates the value
+ *
+ * @n: int to check
+ *
+ * Return: 0 is success
+ */
 void reset_to_98(int *n)
 {
+
 	*n = 98;
 }
