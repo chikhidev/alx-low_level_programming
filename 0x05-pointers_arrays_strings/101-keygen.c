94ff529a0d47f83b3ec026f3c506ed58d0cf463d
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-27 17:59:55 -0700

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,19 +8,20 @@
  * Description: Program that generates random valid passwords
  * Return: 0 if works
  */
+
 int main(void)
 {
-	int i = 0, random, sum;
+	int i = 0, random, checksum;
 
 	srand(time(NULL));
-	sum = 2772;
-	while (sum > 122)
+	checksum = 2772;
+	while (checksum > 122)
 	{
 		random = (rand() % 100);
 		printf("%c", random);
-		sum -= random;
+		checksum -= random;
 		i++;
 	}
-	printf("%c", sum);
+	printf("%c", checksum);
 	return (0);
 }
