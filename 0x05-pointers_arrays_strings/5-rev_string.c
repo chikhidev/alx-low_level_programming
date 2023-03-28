69784ee819c6a6b5cd3acc0a8690685d34bd6578
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-27 17:25:59 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string
+ * @s: char to check
+ *
+ * Return: 0 is success
+ */
+void rev_string(char *s)
+{
+	int a = 0, b, c;
+	char d;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
