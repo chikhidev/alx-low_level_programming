3c54fe3a82ff712ae575ee121b058475831cc896
Abderrahim0Dev <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-03-30 15:52:41 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
     int i = 0;
 
-    while (i < n && *(src + i) != '\0')
+    while (i < n && src[i] != '\0')
     {
-        *(dest + i) = *(src + i);
+        dest[i] = src[i];
         i++;
     }
 
     while (i < n)
     {
-        *(dest + i) = '\0';
+        dest[i] = '\0';
         i++;
     }
 
