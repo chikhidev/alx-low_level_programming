2cec9538819f3e9c18e4272f6403709011ce8fc1
Abderrahim0Dev <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-03-30 18:06:10 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
     while (i < n)
     {
         if (src[i] == '\0')
+        {
             break;
+        }
         dest[i] = src[i];
         i++;
     }
