ddd24f0c49d4aae3ce837e2fca296cce16857cb0
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-30 03:36:35 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
-	while ( dest[i] != '\0' )
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
- 	
-	while ( src[j] != '\0' )
+	while (src[j] != '\0')
        	{
 		dest[i] = src[j];
 		j++;
