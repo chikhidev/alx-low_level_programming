19754bd8a2bb85b5c9099cff80199291e7e588dd
Abderrahim0Dev <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-03-31 01:18:17 +0000

    104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,78 +2,83 @@
 #include <stdio.h>
 
 /**
- * isPrintableASCII - determines if n is a printable ASCII char
- * @n: integer
- * Return: 1 if true, 0 if false
- */
+* isPrintableASCII - determines if n is a printable ASCII char
+* @n: integer
+* Return: 1 if true, 0 if false
+*/
 int isPrintableASCII(int n)
 {
-	return (n >= 32 && n <= 126);
+return (n >= 32 && n <= 126);
 }
 
 /**
- * printHexes - print hex values for string b in formatted form
- * @b: string to print
- * @start: starting position
- * @end: ending position
- */
+* printHexes - print hex values for string b in formatted form
+* @b: string to print
+* @start: starting position
+* @end: ending position
+*/
 void printHexes(char *b, int start, int end)
 {
-	int i = 0;
+int i = 0;
 
-	while (i < 10)
-	{
-		if (i < end)
-			printf("%02x", *(b + start + i));
-		else
-			printf("  ");
-		if (i % 2)
-			printf(" ");
-		i++;
-	}
+while (i < 10)
+{
+if (i < end)
+printf("%02x", *(b + start + i));
+else
+{
+printf("  ");
+}
+if (i % 2)
+{
+printf(" ");
+}
+i++;
+}
 }
 
 /**
- * printASCII - print ascii values for string b,
- * formatted to replace nonprintable chars with '.'
- * @b: string to print
- * @start: starting position
- * @end: ending position
- */
+* printASCII - print ascii values for string b,
+* formatted to replace nonprintable chars with '.'
+* @b: string to print
+* @start: starting position
+* @end: ending position
+*/
 void printASCII(char *b, int start, int end)
 {
-	int ch, i = 0;
+int ch, i = 0;
 
-	while (i < end)
-	{
-		ch = *(b + i + start);
-		if (! (ch >= 32 && ch <= 126))
-			ch = 46;
-		printf("%c", ch);
-		i++;
-	}
+while (i < end)
+{
+ch = *(b + i + start);
+if (! (ch >= 32 && ch <= 126))
+ch = 46;
+printf("%c", ch);
+i++;
+}
 }
 
 /**
- * print_buffer - prints a buffer
- * @b: string
- * @size: size of buffer
- */
+* print_buffer - prints a buffer
+* @b: string
+* @size: size of buffer
+*/
 void print_buffer(char *b, int size)
 {
-	int s = 0, e;
+int s = 0, e;
 
-	if (size > 0)
-	{
-		while (s < size)
-		{
-			e = (size - s < 10) ? size - s : 10;
-			printf("%08x: ", s);
-			printHexes(b, s, e);
-			printASCII(b, s, e);
-			printf("\n");
-      s += 10;
-		}
-	} else
-		printf("\n");
+if (size > 0)
+{
+while (s < size)
+{
+e = (size - s < 10) ? size - s : 10;
+printf("%08x: ", s);
+printHexes(b, s, e);
+printASCII(b, s, e);
+printf("\n");
+s += 10;
+}
+}
+else
+printf("\n");
 }
