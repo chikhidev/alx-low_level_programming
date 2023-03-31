533e5a6554b8dcceaace2a86d0ee2ee96c8d1a20
abderrahim chikhi <chikhi.dev@gmail.com>
2023-03-30 17:30:17 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str: pointer to the string to be capitalized
+ *
+ * Return: pointer to the resulting string
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (i == 0)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+if (str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == '\t' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] = str[i + 1] - 32;
+}
+}
+}
+i++;
+return (str);
+}
+
