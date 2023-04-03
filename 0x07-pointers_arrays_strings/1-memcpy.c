d1a954ce84fc21b83450c43eb4cfe329f48036ab
Abderrahim Chikhi <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-04-03 23:04:06 +0000

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,14 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i = 0;
+unsigned int i = 0;
 
-    while (i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
 
-    return (dest);
+return (dest);
 }
 
