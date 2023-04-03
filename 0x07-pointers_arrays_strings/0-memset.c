6d481176f1350c88123150ae1958212089864ffb
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-03 15:11:56 -0700

    9

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
- unsigned int i;
+int i = 0;
 
- for (i = 0; i < n; i++)
- {
- s[i] = b;
- }
- return (s);
+while (i < n) {
+s[i] = b;
+i++;
+}
+return (s);
 }
 
