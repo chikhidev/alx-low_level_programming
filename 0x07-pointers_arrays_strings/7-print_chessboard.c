379419d01e91fec1cd262588d48c6316cdd212ab
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-03 17:23:07 -0700

    7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_chessboard - print the chessboard, 2d array
+* @a: 2d array of chars
+*Return: void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar (a[i][j]);
+			j++;
+		}
+		_putchar ('\n');
+		i++;
+	}
+}
