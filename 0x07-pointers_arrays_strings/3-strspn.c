fbd4c56e81cf20ecba02e9bdbfc57f6bc287c5d9
Abderrahim Chikhi <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-04-04 00:06:59 +0000

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strspn - function that gets the length of a prefix substring
- *
- * @accept: pointer to get the bytes
- * @s: pointer to string char type
- *
- * Return: the number of bytes in the initial segment of s which consist only of bytes from accept
- */
+* _strspn - function that gets the length of a prefix substring
+*
+* @accept: pointer to get the bytes
+* @s: pointer to string char type
+*
+* Return: the number of bytes in the initial segment of s which consist only of bytes from accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int a = 0, b;
