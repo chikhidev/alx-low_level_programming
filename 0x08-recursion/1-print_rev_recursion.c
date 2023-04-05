00ff1632aae51c3786bedd48288677dcf3213943
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-05 12:41:56 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion -  function that prints a string in reverse.
+* @s: string given
+* Returns nothing
+*/
+
+void _print_rev_recursion(char *s)
+{
+  if (*s)
+  {
+    s++;
+    _print_rev_recursion(s);
+    _putchar(*s);
+  }
+  else
+  {
+	  _putchar('\n');
+  }
+}
