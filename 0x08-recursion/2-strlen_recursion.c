bc50e54cfe5111c1ec66008b275743abb47da296
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-05 15:57:16 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,13 @@
 /**
 * _strlen_recursion - function that returns the length of a string.
 * @s: string given
-*Returns: integer of strig length
+* Returns: integer of strig length
 */
 
 int _strlen_recursion(char *s)
 {
-if (*s) {
+if (*s)
+{
 return (1 + _strlen_recursion(s + 1));
 }
 return (0);
