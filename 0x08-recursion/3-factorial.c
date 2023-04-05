822c0b74393986f20272c53fc404810e58f45d46
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-05 16:29:20 -0700

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,14 +11,14 @@ int factorial(int n)
 {
 if (n == 0)
 {
-return 1;
+return (1);
 }
 else if (n > 0)
 {
-return n * factorial(n - 1);
+return (n * factorial(n - 1));
 }
 else
 {
-return -1;
+return (-1);
 }
 }
