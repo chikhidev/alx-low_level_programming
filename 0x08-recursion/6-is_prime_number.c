36c6031c15dfc71bf00c08f83e1a20cf48b676d9
Abderrahim Chikhi <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-04-06 01:35:23 +0000

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,11 +10,17 @@
 
 int is_prime(int n, int counter) {
 if (n == counter)
+{
 return (1);
+}
 else if (n % counter == 0)
+{
 return (0);
+}
 else
+{
 return (is_prime(n, counter + 1));
+}
 }
 
 /**
@@ -26,7 +32,11 @@ return (is_prime(n, counter + 1));
 
 int is_prime_number(int n) {
 if (n <= 1)
+{
 return (0);
+}
 else
+{
 return is_prime(n, 2);
+}
 }
