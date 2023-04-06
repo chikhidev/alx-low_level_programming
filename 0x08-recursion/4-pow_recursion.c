1f4ed0709d63dcecc2bca35161972852911b3265
Abderrahim Chikhi <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-04-06 00:26:03 +0000

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,11 +10,17 @@
 
 int _pow_recursion(int x, int y)
 {
-    	if (y < 0)
-        	return (-1);
-    	if (y == 0)
-        	return (1);
-	else
-		return (x * _pow_recursion(x, y - 1));
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
 }
 
