0b8019ae604eefe21802e168b25d809c7e0c1681
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-05 18:33:03 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime - function that check if there's any other deviser
+ *
+ * @n: the number given
+ * @counter: that countes from 2 to the number
+ * Return: 1 on success
+ */
+
+int is_prime(int n, int counter) {
+if (n == counter)
+return (1);
+else if (n % counter == 0)
+return (0);
+else
+return (is_prime(n, counter + 1));
+}
+
+/**
+ * is_prime_number - function that returns if number is prime
+ *
+ * @n: number given
+ * Return: 1 on sucess
+ */
+
+int is_prime_number(int n) {
+if (n <= 1)
+return (0);
+else
+return is_prime(n, 2);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,4 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
