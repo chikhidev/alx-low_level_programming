cb305b854e7c1be13c00b218a4b9174a08394dc2
Abderrahim Chikhi <126016947+Abderrahim0Dev@users.noreply.github.com>
2023-04-06 01:03:19 +0000

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,15 @@
 
 int equelizer(int n, int x)
 {
-    if (x * x == n)
-        return (x);
-    else if (x * x > n)
-        return (-1);
-    return (equelizer(n, x + 1));
+if (x * x == n)
+{
+return (x);
+}
+else if (x * x > n)
+{
+return (-1);
+}
+return (equelizer(n, x + 1));
 }
 
 /**
@@ -26,6 +30,6 @@ int equelizer(int n, int x)
 
 int _sqrt_recursion(int n)
 {
-    return equelizer(n, 1);
+return equelizer(n, 1);
 }
 
