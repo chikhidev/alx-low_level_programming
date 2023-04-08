b3ee03c90c548921b7fef249846cf042de21cc6c
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-08 07:23:24 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+/**
+ * main - main function of the programe
+ * @argc: arguments coutn
+ * @argv: arguments array
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
