c9eb34acad8c65f2a943c0d5b841fa24640db69d
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-08 07:33:29 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: counter arg
+ * @argv: args array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d", argc);
+	return (0);
+}
