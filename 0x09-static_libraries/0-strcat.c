fb028e3ac890f41d2f3385288feb107fbc01fe1f
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-09 11:10:20 -0700

    0

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcat - appends the src string to the dest string
+ *
+ * @dest: char to check
+ * @src: char to check
+ * Description: This will append the src string to the dest string
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _strspn - Locates a character in a string
+* @s: This is the main C string to be scanned.
+* @accept: This is the string containing the list of characters to match in s
+* Return: return count
+**/
+
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0')
+	{
+		j = 0;
+		while (str2[j] != '\0')
+		{
+			if (str2[j] == str1[i])
+			{
+				count++;
+				break;
+			}
+
+			j++;
+		}
+
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	return (count);
+}
