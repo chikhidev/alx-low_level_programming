edb25732cc19d4379abd884ddae2e9e85cda6336
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-11 19:57:15 -0700

    4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,13 +10,11 @@
 
 void free_grid(int **grid, int height)
 {
-	int i;
+int i;
 
-	if (grid == NULL)
-		free(grid);
-
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-
-	free(grid);
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
 }
