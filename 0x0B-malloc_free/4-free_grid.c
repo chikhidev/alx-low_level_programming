72f92c885a917b6e6e3a438e6db1f9bda934557a
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-11 19:51:33 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - function that frees a 2 dimensional grid previously
+* @grid: grid given
+* @height: height given
+* Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int temp = 0;
+
+	if (grid == NULL || height <= 0)
+		return;
+	while (temp < height)
+	{
+		free(grid[temp]);
+		temp++;
+	}
+	free(grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,3 @@
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
