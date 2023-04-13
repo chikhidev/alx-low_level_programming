22dbb49b2f8a9a1528ae7dcab7aae987a968f5a5
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-12 18:12:58 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * count_words - counts the number of words in a string
+ * @str: pointer to string
+ * Return: number of words in the string
+ */
+int count_words(char *str)
+{
+    int i, count = 0, len;
+
+    for (len = 0; *(str + len); len++)
+        ;
+
+    i = 0;
+    while (i < len)
+    {
+        while (str[i] == ' ' && i < len)
+            i++;
+        if (i == len)
+            break;
+        count++;
+        while (str[i] != ' ' && i < len)
+            i++;
+    }
+    return (count);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: pointer to string
+ * Return: double pointer to array of pointers
+ */
+char **strtow(char *str)
+{
+    int i, j, len, wordsize, count;
+    char **p;
+
+    if (str == NULL || *str == '\0')
+        return (NULL);
+
+    len = count_words(str);
+    p = malloc((len + 1) * sizeof(char *));
+    if (p == NULL)
+        return (NULL);
+
+    i = j = 0;
+    while (i < len)
+    {
+        while (str[j] == ' ')
+            j++;
+        wordsize = 0;
+        while (str[j + wordsize] != ' ' && str[j + wordsize] != '\0')
+            wordsize++;
+        p[i] = malloc((wordsize + 1) * sizeof(char));
+        if (p[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+                free(p[j]);
+            free(p);
+            return (NULL);
+        }
+        for (wordsize = 0; str[j] != ' ' && str[j] != '\0'; j++, wordsize++)
+            p[i][wordsize] = str[j];
+        p[i][wordsize] = '\0';
+        i++;
+    }
+    p[i] = NULL;
+    return (p);
+}
+
