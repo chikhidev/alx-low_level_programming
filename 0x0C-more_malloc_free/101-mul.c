026403df88e8af23434cf4bcc71375a759f51b67
Abderrahim Chikhi <126016947+chikhidev@users.noreply.github.com>
2023-04-14 18:48:20 +0000

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,4 +10,37 @@
  * Return: int
 **/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+int i = 1, j;
+unsigned long M;
+
+if (argc != 3)
+{
+_putchar("Error\n");
+exit(98);
+}
+
+while (i < argc)
+{
+j = 0;
+while (argv[i][j] != '\0')
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{
+_putchar("Error\n");
+exit(98);
+}
+j++;
+}
+i++;
+}
+
+M = atol(argv[1]) * atol(argv[2]);
+printf("%lu\n", M);
+return (0);
+}
 
