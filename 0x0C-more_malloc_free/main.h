38c33dcc529471bea0cbaacd22f91fdbf655f51c
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-14 06:32:29 -0700

    PUSHED_SUCESS

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdlib.h>
+
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
