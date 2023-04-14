3b0447e2074c60e12e1b15d9540102dd3a4c72cb
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-14 06:38:58 -0700

    PUSHED_SUCESS

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - function that allocates memory using malloc.
@@ -6,7 +7,7 @@
  * Return:char
  */
 
-void *malloc_checked(size_t b)
+void *malloc_checked(unsigned int b)
 {
 void *p = malloc(b);
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,1 @@
-#ifndef MAIN
-#define MAIN
-
-#include <stdlib.h>
-
 void *malloc_checked(unsigned int b);
-
-#endif
