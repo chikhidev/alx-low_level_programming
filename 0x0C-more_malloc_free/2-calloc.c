24d24463be4e3e6ab82d239c49710f897acf7856
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-14 08:13:26 -0700

    PUSHED_SUCESS

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if ((nmemb || size) == 0)
 		return (NULL);
 	p = calloc(nmemb, size);
-	if (!p)
+	if (p == NULL)
 		return (NULL);
 	return (p);
 }
