867b72efa36c02a7bc39f076dd821e77cbff65d7
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-14 11:42:24 -0700

    PUSHED_SUCESS

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+**/
+
+
