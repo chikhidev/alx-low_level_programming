f7e5c5c387c96c107778e9225afe5418be98c0d5
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-13 17:14:10 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: bytes to reserve
+ * Return:char
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *temp = malloc(b);
+	if (temp == NULL)
+		return exit(98);
+	return (temp);
+}
