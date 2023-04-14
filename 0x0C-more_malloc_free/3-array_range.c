8d74c3fbe14d3523ae8ecffefa123d76063473c2
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-14 08:58:09 -0700

    PUSHED_SUCESS

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - fujnction returns array range
+ * @min: int to start
+ * @max: int to end
+ * Return: array pointer
+ */
+
+int *array_range(int min, int max) {
+  int i = min;
+  if (min > max)
+    return (NULL);
+  int *p = malloc(sizeof(int) * (max - min));
+  if (!p)
+    return (NULL);
+  while (i <= max) {
+    *(p + i) = i;
+    i++;
+  }
+  return (p);
+}
