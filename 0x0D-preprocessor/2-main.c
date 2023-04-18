34c70c84d31ac537d783238053639c7c7e6913a0
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-17 18:16:28 -0700

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
