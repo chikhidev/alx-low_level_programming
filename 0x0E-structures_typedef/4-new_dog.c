238b0a166fd4b411416290504ab767a0ea029910
Abderrahim Chikhi <126016947+chikhidev@users.noreply.github.com>
2023-04-18 01:55:36 +0000

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,19 +14,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 int nameCount = 0, ownerCount = 0;
 dog_t *new_dog = malloc(sizeof(dog_t));
-
+  
 if (new_dog == NULL)
 return (new_dog);
 
 while (name[nameCount])
 nameCount++;
-
 while (owner[ownerCount])
 ownerCount++;
-
 new_dog->name = malloc((nameCount + 1) * sizeof(char));
 new_dog->owner = malloc((ownerCount + 1) * sizeof(char));
-
 if (new_dog->name == NULL || new_dog->owner == NULL)
 {
 free(new_dog->name);
@@ -34,24 +31,20 @@ free(new_dog->owner);
 free(new_dog);
 return (NULL);
 }
-
 nameCount = 0;
 while (name[nameCount])
 {
 new_dog->name[nameCount] = name[nameCount];
 nameCount++;
 }
-
 ownerCount = 0;
 while (owner[ownerCount])
 {
 new_dog->owner[ownerCount] = owner[ownerCount];
 ownerCount++;
 }
-
 new_dog->name[nameCount] = 0;
 new_dog->owner[ownerCount] = 0;
 new_dog->age = age;
-
 return (new_dog);
 }
