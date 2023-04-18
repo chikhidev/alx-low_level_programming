2d5eca7d0dbe9f908b977c3ebaac316c9f363caa
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-17 18:40:41 -0700

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,12 @@ typedef struct dog
 	char *owner;
 }
 	dog;
-typedef struct dog type_dog;
+typedef struct dog dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-type_dog *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog *d);
 
 #endif
