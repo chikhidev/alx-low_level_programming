97656f778519f27b99a8ee6a23817d8d9660bb09
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-19 11:40:54 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointersi.h"
 
+/**
+ * print_name - function that prints string given
+ * @name: pointer
+ * @f: function pointer
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	(f)(name);
