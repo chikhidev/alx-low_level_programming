47789ff5cf43d53f602665358c184cf5629b7d2e
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-19 15:27:15 -0700

    0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
 #include <stddef.h>
+/* stddef.h header defines various types and macros */
+/* size_t is unsigned integral type and is the result of the sizeof keyword */
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
 #endif
