f256717a9f928bbdaa9721e34f66ea6913cc3831
Abderrahim Chikhi <chikhi.dev@gmail.com>
2023-04-19 23:15:51 +0000

    3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,7 +11,7 @@
  */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 
 /**
@@ -24,7 +24,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 
 /**
@@ -37,7 +37,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 
 /**
@@ -50,7 +50,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	return (a / b);
+return (a / b);
 }
 
 /**
@@ -63,6 +63,6 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	return (a % b);
+return (a % b);
 }
 
