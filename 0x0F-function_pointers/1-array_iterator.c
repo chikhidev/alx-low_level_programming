f409ad7f347d7abdb58ec17ffae5380cdee99c7a
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-19 15:45:51 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function calls a function each element
+ * @array: array given pinter
+ * @size: size of array
+ * @action: action to call
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+	if (array && size && action)
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+}
+
