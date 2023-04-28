d24c846aa7a56ea75e407eeb56b1ad29e238894f
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-27 17:49:52 -0700

    linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
 * Return: void
 */
 
-void __attribute__ ((constructor)) before_main (void)
+void __attribute__ ((constructor)) before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
