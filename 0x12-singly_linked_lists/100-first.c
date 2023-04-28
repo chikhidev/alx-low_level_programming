e4acc620dc0bf1c2b3ec192af4510c2f86401d17
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-27 17:45:22 -0700

    linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* before_main - before the main function is executed.
+* Return: void
+*/
+
+void __attribute__ ((constructor)) before_main (void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
