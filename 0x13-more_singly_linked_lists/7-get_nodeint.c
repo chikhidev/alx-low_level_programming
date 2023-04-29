bc0543eaef3c2164747153b1b097b4d2d0084772
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-29 13:30:29 -0700

    linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,19 +11,19 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	if (!head)
 		return (NULL);
-	
+
 	while (i != index)
 	{
 		head = head->next;
-		
+
 		if (!head)
 			return (NULL);
-		
+
 		i++;
 	}
-	
+
 	return (head);
 }
