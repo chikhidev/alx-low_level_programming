229da7ca011c7fe91c19b706a13fc717dfdae1f4
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-29 14:07:39 -0700

    linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,10 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new_node, *curr;
 
-if (!n)
-return (NULL);
-
-new_node = (listint_t *) malloc(sizeof(listint_t));
+new_node = (listint_t*) malloc(sizeof(listint_t));
 
 if (!new_node)
 return (NULL);
