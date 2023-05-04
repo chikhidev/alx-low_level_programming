1009d20794a4eea37e41fb5b9c2243c6a1027b04
abderrahim chikhi <chikhi.dev@gmail.com>
2023-05-04 16:54:56 -0700

    linked list

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,24 +5,28 @@
  * @n: int
  * Return:void
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << 63;
+	unsigned long int c = 1;
+	unsigned int i = 0;
 
+	c <<= 63;
+	
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
 
-	while (mask > 0)
+	while (c > 0)
 	{
-		if (n & mask)
-			_putchar('1');
-		else
+		if ((n & c) == 0 && i == 1)
 			_putchar('0');
-		mask >>= 1;
+		
+		if ((n & c))
+		{
+			_putchar('1');
+			i = 1;
+		}
+
+		c = c >> 1;
+		
 	}
 }
-
