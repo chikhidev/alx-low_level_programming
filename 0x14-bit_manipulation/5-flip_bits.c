a8a6bda3ef14eebe33a934b79421bfa389e48b52
abderrahim chikhi <chikhi.dev@gmail.com>
2023-05-04 17:13:17 -0700

    linked list

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - number of bits you would need to flip to get from one number to another.
+ * flip_bits - number of bits you would need to flip to
  * @n: u l int
  * @m: u l int
  * Return: u int
