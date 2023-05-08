6e902ac39011388ce65dce5e344ffd56d73c4f3e
abderrahim chikhi <chikhi.dev@gmail.com>
2023-05-08 14:13:00 -0700

    linked list

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			i++;
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	
+
 	w = write(fd, text_content, i);
 
 	if (fd == -1 || w == -1)
