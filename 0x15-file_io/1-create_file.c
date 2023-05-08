b585cf50adfd3b5fc07d6ac3ca06f4e8f100b966
abderrahim chikhi <chikhi.dev@gmail.com>
2023-05-08 14:01:42 -0700

    linked list

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * @filename: file to read.
+ * @text_content: content
+ * Return: number of charachets
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, i = 0, w;
+
+if (!filename)
+return (-1);
+
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+
+if (fd == -1)
+return (-1);
+
+if (text_content)
+{
+while (text_content[i])
+i++;
+
+w = write(fd, text_content, i);	
+}
+
+if (w == -1)
+return (-1);
+
+close(fd);
+
+return (1);
+}
