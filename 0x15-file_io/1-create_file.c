966664fe4521818cf3d515edc8af52754a616c11
abderrahim chikhi <chikhi.dev@gmail.com>
2023-05-08 14:06:41 -0700

    linked list

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX
+ * create_file - creats file
  * @filename: file to read.
  * @text_content: content
  * Return: number of charachets
