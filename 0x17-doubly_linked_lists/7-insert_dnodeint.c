74bdfb81317a87cb3fac51843ab8e17f4dea6406
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 15:11:23 -0700

    models

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *curr = *h, *new_node, *next;
+	unsigned int index = 0;
+
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (*h);
+	
+	new_node->n = n;
+
+	if (idx == 0 || *h == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*h = new_node;
+		return (*h);
+	}
+
+	while (curr && index != idx)
+	{
+		curr = curr->next;
+		index++;
+	}
+	curr = curr->prev;
+	/* after this the current node will be at the position*/
+
+	next = curr->next;
+	curr->next = new_node;
+	new_node->prev = curr;
+	new_node->next = next;
+	next->prev = new_node;
+
+	return (*h);
+}
