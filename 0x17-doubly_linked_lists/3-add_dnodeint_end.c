72ae11e3d74259cc1ef2d16b67378289a47509c1
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 11:21:34 -0700

    models

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - functions adds node at the end of the list
+ * @head: head pointer
+ * @n: number int data
+ * Return: head
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *curr = *head, new_node;
+
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (*head);
+	
+	new_node->n = n;
+	new_node->next = NULL;
+	
+	if (!head)
+		new_node->prev = NULL;
+	else
+	{
+		while (curr)
+			curr = curr->next;
+		/* after this curr is the last one (at least for now)*/
+		curr->next = new_node;
+		new_node->prev = curr;
+	}
+	*head = new_node;
+
+	return (*head);
+}
