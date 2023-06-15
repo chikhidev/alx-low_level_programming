a3436521844035b10bcb3465ecf67152621b8853
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 11:31:32 -0700

    models

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,10 +14,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (*head);
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
-	
+
 	if (!*head)
 	{
 		new_node->prev = NULL;
