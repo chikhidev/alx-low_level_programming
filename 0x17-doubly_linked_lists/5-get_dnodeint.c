944c2688ea3b0769dc51ad20b04e309b4382ea62
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 12:56:34 -0700

    models

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - finds node at index
+ * @head: head pointer
+ * @index: index
+ * Return: nod
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 	dlistint_t *curr = head;
-	
+
 	while (curr)
 	{
 		if (counter == index)
