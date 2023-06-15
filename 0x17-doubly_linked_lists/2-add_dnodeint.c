e9cd6db044ddb4c87e628f127452e552fdb7f3be
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 11:10:00 -0700

    models

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,11 +21,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		new_node->next = NULL;
-		return (new_node);
+		*head = new_node;
 	}
-
-	(*head)->prev = new_node;
-	new_node->next = (*head);
+	else
+	{
+		(*head)->prev = new_node;
+		new_node->next = (*head);
+		*head = new_node;
+	}
 
 	return (*head);
 }
