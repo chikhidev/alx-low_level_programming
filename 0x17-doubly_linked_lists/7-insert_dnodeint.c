747328744f40f5a03611377552af6c8ce52b8831
Abdoo-ちき <chikhi.dev@gmail.com>
2023-06-15 23:27:52 +0100

    UPDATE

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,44 +10,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *curr = *h, *new_node, *next;
-    unsigned int index = 0;
+dlistint_t *curr = *h, *new_node, *next;
+unsigned int index = 0;
 
-    new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return NULL;
+new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+if (!new_node)
+return NULL;
 
-    new_node->n = n;
+new_node->n = n;
 
-    if (idx == 0 || *h == NULL)
-    {
-        new_node->next = *h;
-        new_node->prev = NULL;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return *h;
-    }
+if (idx == 0 || *h == NULL)
+{
+new_node->next = *h;
+new_node->prev = NULL;
+if (*h != NULL)
+(*h)->prev = new_node;
+*h = new_node;
+return *h;
+}
 
-    while (curr && index != idx - 1)
-    {
-        curr = curr->next;
-        index++;
-    }
+while (curr && index != idx - 1)
+{
+curr = curr->next;
+index++;
+}
 
-    if (curr == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
+if (curr == NULL)
+{
+free(new_node);
+return NULL;
+}
 
-    next = curr->next;
-    curr->next = new_node;
-    new_node->prev = curr;
-    new_node->next = next;
-    if (next != NULL)
-        next->prev = new_node;
+next = curr->next;
+curr->next = new_node;
+new_node->prev = curr;
+new_node->next = next;
+if (next != NULL)
+next->prev = new_node;
 
-    return *h;
+return *h;
 }
 
