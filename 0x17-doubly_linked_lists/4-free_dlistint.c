92998e494c5d5bf044a23b86577594a1dafaf4fb
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 11:40:35 -0700

    models

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - function that frees all nodes from list
+ * @head: head pointer
+ * Return: void
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *curr = head, *next = NULL;
