5460372d75af2023abfeb1ad84a7236876449363
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 09:40:00 -0700

    models

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/*
+ * dlistint_len - function that returns elements in a linked list
+ * @h: header pointer
+ * Return: counter
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+	const dlistint_t curr = h;
+
+	while (curr != NULL)
+	{
+		curr = curr->next;
+		counter++;
+	}
+
+	return (counter);
+}
