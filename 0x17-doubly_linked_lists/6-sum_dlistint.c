bc216e0780205a4bfdabdc3cbedfca4b871206b3
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-15 13:03:17 -0700

    models

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *curr = head;
+
+	while (curr)
+	{
+		sum += curr->n;
+		curr = curr->next;
+	}
+
+	return (sum);
+}
