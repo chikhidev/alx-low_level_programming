3141c88f5f826e21c9829693828639724e435fe0
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-19 11:35:42 -0700

    models

---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isdigit - checkes if c is digit
+* @c: variable param
+* Return: 1 if true 0 if false
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
