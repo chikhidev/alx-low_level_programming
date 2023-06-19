f93bbcb8800244e0eb6ef196bb5a73ed580b17a0
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-19 11:33:52 -0700

    models

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char holberton[] = "_putchar";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
