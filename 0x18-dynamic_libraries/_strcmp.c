bdd8d37439bebf33baabc058e30685f8c5910d6d
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-19 11:42:44 -0700

    models

---
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: difference between the ASCII values of the first non-matching
+ *         characters of the two strings, or 0 if the strings are identical
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
