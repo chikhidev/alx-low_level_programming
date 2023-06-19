132f7d514020efadd716fda988cc3c47bf069d06
abderrahim chikhi <chikhi.dev@gmail.com>
2023-06-19 11:44:32 -0700

    models

---
+++ 0x18-dynamic_libraries/_strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr- a function find char
+* @s: string given
+* @c: char to look for
+*
+* Return: returns a pointer to the first occurance
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (c == 0)
+		return (s + i);
+	return (0);
+}
